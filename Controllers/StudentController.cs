70bf64ff41cda93d4da91ad6c3d810844d1d396c
Maksym652 <77639043+Maksym652@users.noreply.github.com>
2021-05-27 17:25:31 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework2_Basecamp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        static List<Student> students = new List<Student>()
+        {
+            new Student(1, "Ivanenko", 201, 122, true),
+            new Student(2, "Petrenko", 202, 122, false),
+            new Student(3, "Vasylenko", 205, 123, true),
+            new Student(4, "Stepanenko", 208, 121, false),
+            new Student(5, "Kovalenko", 209, 121, true)
+        };
+        
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Student Get([FromRoute] int id)
+        {
+            return students.Find(student => student.Id == id);
+        }
+
+        [HttpPost]
+        [Route("/post")]
+        public void Post([FromQuery] int id, [FromQuery] string name, [FromQuery] int group, [FromQuery] int specialty, [FromQuery] bool isStudiedOnBudget)
+        {
+            students.Add(new Student(id, name, group, specialty, isStudiedOnBudget));
+        }
+
+        [HttpPut]
+        [Route("/put")]
+        public void Put([FromQuery] int id, [FromQuery] string name, [FromQuery] int group, [FromQuery] int specialty, [FromQuery] bool isStudiedOnBudget)
+        {
+            Student st = students.Find(student => student.Id == id);
+            st.Name = name;
+            st.Group = group;
+            st.Specialty = specialty;
+            st.IsStudiedOnBudget = isStudiedOnBudget;
+        }
+
+        [HttpDelete]
+        [Route("/delete")]
+        public void Delete([FromBody] int id)
+        {
+            students.Remove(students.Find(st => st.Id == id));
+        }
+    } 
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework2_Basecamp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Group { get; set; }
+        public int Specialty { get; set; }
+        public bool IsStudiedOnBudget { get; set; }
+        
+        public Student(int id, string name, int group, int specialty, bool isStudiedOnBudget)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Group = group;
+            this.Specialty = specialty;
+            this.IsStudiedOnBudget = isStudiedOnBudget;
+        }
+    }
+}
